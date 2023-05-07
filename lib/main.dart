4dad9ea227ce01c83ec8d0f887a0b10b92d88e85
zainahmed0597 <zainahmed0597@gmail.com>
2023-05-07 09:45:02 +0500

    badge is working now

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Product List',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.blue,
       ),
       home: const ProductListScreen(),
     );
--- lib/product_list.dart
+++ lib/product_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:badges/badges.dart';
+import 'package:badges/badges.dart' as badges;
 
 class ProductListScreen extends StatefulWidget {
   const ProductListScreen({Key? key}) : super(key: key);
@@ -13,13 +13,27 @@ class _ProductListScreenState extends State<ProductListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Product List"),
+        title: const Text('Product List'),
         centerTitle: true,
         actions: [
-
-          Icon(Icons.shopping_bag_outlined), SizedBox(width: 20.0,)
-
+          Center(
+            child: badges.Badge(
+              badgeContent: const Text('0', style: TextStyle(
+                color: Colors.white,
+              ),),
+              badgeAnimation: const badges.BadgeAnimation.slide(animationDuration: Duration(milliseconds: 300)),
+              position: badges.BadgePosition.topEnd(top: 0, end: 0),
+              child: IconButton(
+                icon: const Icon(Icons.shopping_bag_outlined),
+                onPressed: () {},
+              ),
+            ),
+          ),
+          const SizedBox(width: 20.0,),
         ],
+
+
+
       ),
     );
   }
